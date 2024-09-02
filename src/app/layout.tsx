b9e0a4af49f5045dfbe89e55cc6951edbda0c5f3
itspornpailin <pornpailin_j@cmu.ac.th>
2024-09-02 15:52:05 +0700

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,9 +34,9 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Footer
-            year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            year="2024"
+            fullName="Pornpailin Jaowatthanaphong"
+            studentId="660610777"
           />
         </div>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import Header from "@components/Header";
-import Navbar from "@components/Navbar";
-import Footer from "@components/Footer";
-
 export default function Home() {
   return (
     <div>
